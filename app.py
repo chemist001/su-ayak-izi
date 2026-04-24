65ff549db85ca1f1bf0e727099b8ada5a4e3c0d4
chemist001 <erdempolat9001@gmail.com>
2026-04-24 11:30:03 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import base64
 import matplotlib.pyplot as plt
 import io
 from google import genai
-client = genai.Client(api_key="AIzaSyAMQYH5_RuSI5F5U3FLMRRJlsOF-DUQTLA")
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"].strip())
 
 # --- KÜTÜPHANE KONTROLLERİ ---
 try:
