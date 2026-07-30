277159d4a1d1ca5c4c0c615819b5e8d0a8da1f91
chemist001 <erdempolat9001@gmail.com>
2026-07-30 17:09:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -840,7 +840,7 @@ def show_calculator_page():
 def sayfa_veri_kalitesi():
     st.subheader("Veri Kalitesi Metodoloji Açıklaması")
 
-    st.info("💡 Firmanızın veri kaynakları ve veri kalitesine ilişkin metodolojik açıklamaları **Veri Kalitesi** bölümünde detaylandırılabilir. Bu alanın boş bırakılması durumunda, raporda **standart metin** geçerli olacaktır.")
+    st.info("💡 Firmanızın veri kaynakları ve veri kalitesine ilişkin metodolojik açıklamaları **Veri Kalitesi** bölümünde detaylandırabilirsiniz. Bu alanın boş bırakılması durumunda, raporda **standart metin** geçerli olacaktır.")
     
     if 'veri_kalitesi_aciklama' not in st.session_state:
         st.session_state['veri_kalitesi_aciklama'] = ""
